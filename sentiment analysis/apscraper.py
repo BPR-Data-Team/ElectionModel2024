038e848d3b32da9e916d79a1bf5c4958abbd3bda
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2023-10-13 10:58:27 -0400

    "sentence by sentence processing"

--- sentiment analysis/apscraper.py
+++ sentiment analysis/apscraper.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import nltk
 nltk.downloader.download('vader_lexicon')
+nltk.download('punkt')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import sent_tokenize
 import spacy
-nlp = spacy.load("en_core_web_sm")
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+from spacy.lang.en.examples import sentences 
+nlp = spacy.load('en_core_web_md')
 sid = SentimentIntensityAnalyzer()
-page = requests.get("https://apnews.com/article/president-joe-biden-ap-poll-prescription-drugs-medicare-b82928109ae8564e750a6a196c98cfe9")
+url = "https://apnews.com/article/president-joe-biden-ap-poll-prescription-drugs-medicare-b82928109ae8564e750a6a196c98cfe9"
+page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 soup.find("div",class_ = "Enhancement-item").clear()
 for x in soup.find_all(): 
@@ -21,7 +22,7 @@ content = soup.find("div",class_ = "RichTextStoryBody RichTextBody")
 for block in content:
     nugget = block.text.strip()
     for sentence in sent_tokenize(nugget):
-        print(sid.polarity_scores(nugget)["compound"])
-        print([tok for tok in nlp(nugget) if (tok.dep_ == "nsubj") ])
-        print(nugget)
+        print(sid.polarity_scores(sentence)["compound"])
+        print([tok for tok in nlp(sentence) if (tok.dep_ == "nsubj") ])
+        print(sentence)
     
