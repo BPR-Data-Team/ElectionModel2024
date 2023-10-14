92781486897b23519d07208e5d2b7a726bff5fcd
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2023-10-14 07:50:03 -0400

    with previous

--- sentiment analysis/apscraper.py
+++ sentiment analysis/apscraper.py
@@ -12,39 +12,41 @@ from spacy.lang.en.examples import sentences
 nlp = spacy.load('en_core_web_md')
 sid = SentimentIntensityAnalyzer()
 # initializing test characteristics
-url = "https://apnews.com/article/president-joe-biden-ap-poll-prescription-drugs-medicare-b82928109ae8564e750a6a196c98cfe9"
+urls = ["https://apnews.com/article/president-joe-biden-ap-poll-prescription-drugs-medicare-b82928109ae8564e750a6a196c98cfe9","https://apnews.com/article/biden-pacific-islands-summit-climate-change-funding-727aa6b908ef5cf1ea28438b965ba9b5","https://apnews.com/article/biden-guns-gun-violence-mass-shootings-5a72e3730b6d24b2143f034a9ee6ed21"]
 demWords = ["biden","democrats", "democrat", "joe biden"]
 demScores = []
 repScores = []
 repWords = ["trump", "donald trump", "republican", "republicans"]
 # process test
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-soup.find("div",class_ = "Enhancement-item").clear() # removing ads and promos from AP News body
-for x in soup.find_all(): # removing empty tags
-      if len(x.get_text(strip=True)) == 0: 
-        x.extract()
-content = soup.find("div",class_ = "RichTextStoryBody RichTextBody")
-for block in content:
-    nugget = block.text.strip()
-    for sentence in sent_tokenize(nugget):
-        # print(sid.polarity_scores(sentence)["compound"])
-        result_list = list(map(str.lower, [tok.text for tok in nlp(sentence) if (tok.dep_ == "nsubj")]))
-        # print(result_list)
+for link in urls:
+  page = requests.get(link)
+  soup = BeautifulSoup(page.content, "html.parser")
+  soup.find("div",class_ = "Enhancement-item").clear() # removing ads and promos from AP News body
+  for x in soup.find_all(): # removing empty tags
+        if len(x.get_text(strip=True)) == 0: 
+          x.extract()
+  content = soup.find("div",class_ = "RichTextStoryBody RichTextBody")
+  for block in content:
+      nugget = block.text.strip()
+      for sentence in sent_tokenize(nugget):
+          # print(sid.polarity_scores(sentence)["compound"])
+          result_list = list(map(str.lower, [tok.text for tok in nlp(sentence) if (tok.dep_ == "nsubj")]))
+          # print(result_list)
 
-        for value in demWords:
-            if value in result_list:
-                demScores.append(sid.polarity_scores(sentence)["compound"])
-                # print("----------------- LOOK HERE ---------------")
-                break
-        for value in repWords:
-            if value in result_list:
-                repScores.append(sid.polarity_scores(sentence)["compound"])
-                # print("----------------- LOOK HERE ---------------")
-                break            
-        print(sentence)
-    
-print(demScores)
-print(sum(demScores) / len(demScores))
-print(repScores)
-print(sum(repScores) / len(repScores))
+          for value in demWords:
+              if value in result_list:
+                  demScores.append(sid.polarity_scores(sentence)["compound"])
+                  # print("----------------- LOOK HERE ---------------")
+                  break
+          for value in repWords:
+              if value in result_list:
+                  repScores.append(sid.polarity_scores(sentence)["compound"])
+                  # print("----------------- LOOK HERE ---------------")
+                  break            
+          # print(sentence)
+      
+  print(soup.title.string)
+  # print(demScores)
+  print((sum(demScores) / len(demScores)))
+  # print(repScores)
+  print((sum(repScores) / len(repScores)))
